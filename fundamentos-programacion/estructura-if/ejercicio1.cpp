9b14d4c2033ee23643b7a265a8acd1788a343c12
Abel Villaseca <abelvillasecac@gmail.com>
2026-02-27 17:42:07 -0500

    ejercicio 1

---
+++ fundamentos-programacion/estructura-if/ejercicio1.cpp
@@ -0,0 +1,20 @@
+// Escribe un programa que solicite la edad de una persona y determine
+// si es mayor de edad (18 años o más) o no.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int edad;
+    cout<<"Ingresa tu edad: ";
+    cin>>edad; //12
+
+    if(edad >= 18){
+      cout<<"Eres mayor de edad."<<endl;
+    }else{
+      cout<<"Eres menor de edad."<<endl;
+    }
+
+    return 0;
+}
